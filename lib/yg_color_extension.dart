2c527bd1b0889f87d397180aed30ee5f1b163dd0
GaoYu <gaoyu.dreamer@gmail.com>
2021-03-17 16:19:55 +0800

    fix: color bug

--- lib/yg_color_extension.dart
+++ lib/yg_color_extension.dart
@@ -1,7 +1,7 @@
 part of dart_extension;
 
 extension YGColorExtension on Color {
-  Color get random {
-    return Color((Random().nextDouble() * 0xFFFFFF).toInt());
+  static Color get random {
+    return Color((Random().nextDouble() * 0xFFFFFF).toInt()).withOpacity(1);
   }
 }
