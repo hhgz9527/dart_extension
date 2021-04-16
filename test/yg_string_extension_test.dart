c347c19ca7584b37d926409b9cc3168aa3fbc43c
GaoYu <gaoyu.dreamer@gmail.com>
2021-04-16 10:26:04 +0800

    test: refactor

--- test/yg_string_extension_test.dart
+++ test/yg_string_extension_test.dart
@@ -2,19 +2,33 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:dart_extension/dart_extension.dart';
 
 void main() {
-  test('remove last chart', () {
-    expect('last'.dropLast, 'las');
+  group('dropLast', () {
+    test('remove last chart', () {
+      expect('last'.dropLast, 'las');
+    });
   });
 
-  test('remove last 3 count chart', () {
-    expect('last'.dropLastWith(3), 'l');
+  group('dropLastWith', () {
+    test('remove last 3 count chart', () {
+      expect('last'.dropLastWith(3), 'l');
+    });
   });
 
-  test('remove first chart', () {
-    expect('first'.dropFirst, 'irst');
+  group('dropFrist', () {
+    test('remove first chart', () {
+      expect('first'.dropFirst, 'irst');
+    });
   });
 
-  test('remove first 3 count chart', () {
-    expect('first'.dropFristWith(3), 'st');
+  group('dropFristWith', () {
+    test('remove first 3 count chart', () {
+      expect('first'.dropFristWith(3), 'st');
+    });
+  });
+
+  group('isEmail', () {
+    test('email check', () {
+      expect('1@qq.com'.isEmail, true);
+    });
   });
 }
