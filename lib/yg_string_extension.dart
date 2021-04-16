e24de236299ecc9776e98e78d5923d447e026d21
GaoYu <gaoyu.dreamer@gmail.com>
2021-04-16 10:25:49 +0800

    add: email check

--- lib/yg_string_extension.dart
+++ lib/yg_string_extension.dart
@@ -9,21 +9,27 @@ extension YGStringExntesion on String {
   }
 
   /// remove string last [count] chart
-  /// 
+  ///
   /// 'last' -> dropLastWith(2) -> 'la'
   String dropLastWith(num count) {
     return this.substring(0, this.length - count);
   }
 
   /// remove string frist chart
-  /// 
+  ///
   /// 'last' -> 'ast'
   String get dropFirst {
     return this.substring(1, this.length);
   }
 
+  bool get isEmail {
+    RegExp exp = RegExp(r"^[\w!#$%&'*+/=?^_`{|}~-]+(?:\.[\w!#$%&'*+/=?^_`{|}~-]+)*@(?:[\w](?:[\w-]*[\w])?\.)+[\w](?:[\w-]*[\w])?");
+    print(this);
+    return exp.hasMatch(this);
+  }
+
   /// remove string first [count] chart
-  /// 
+  ///
   /// 'last' -> dropFristWith(2) -> 'st'
   String dropFristWith(num count) {
     return this.substring(count, this.length);
