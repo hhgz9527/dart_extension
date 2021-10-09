925b946e18cd0f0abba8f65eacb4a7bc534dd915
GaoYu <gaoyu.dreamer@gmail.com>
2021-10-09 14:10:25 +0800

    chore: add start and end of week

--- lib/yg_date_extension.dart
+++ lib/yg_date_extension.dart
@@ -2,18 +2,38 @@ part of dart_extension;
 
 extension YGDateExtension on DateTime {
   /// get this month days count
-  /// 
+  ///
   /// DateTime(2021, 3).monthDay -> 31
   int get monthDays {
     return _daysInMonth(this);
   }
 
   /// date format
-  /// 
+  ///
   /// DateTime.now().format() -> '2021-04-24'
   String format({String pattern = 'yyyy-MM-dd'}) {
     DateFormat formatter = DateFormat(pattern);
     return formatter.format(this);
+  }
+
+  /// get start of week datetime
+  ///
+  /// DateTime.now().startOfWeek
+  DateTime get startOfWeek {
+    DateTime now = DateTime.now();
+    int currentDay = now.weekday;
+    DateTime firstDayOfWeek = now.subtract(Duration(days: currentDay));
+    return DateTime(firstDayOfWeek.year, firstDayOfWeek.month, firstDayOfWeek.day);
+  }
+
+  /// get end of week datetime
+  ///
+  /// DateTime.now().endOfWeek
+  DateTime get endOfWeek {
+    DateTime now = DateTime.now();
+    int currentDay = now.weekday;
+    DateTime endDayOfWeek = now.add(Duration(days: DateTime.daysPerWeek - currentDay));
+    return DateTime(endDayOfWeek.year, endDayOfWeek.month, endDayOfWeek.day);
   }
 
   int _daysInMonth(DateTime date) {
